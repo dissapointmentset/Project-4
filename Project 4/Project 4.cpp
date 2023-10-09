f2e37d108e4dde8012267105d198af69a9c0b16f
Alex <panins895@gmail.com>
2023-10-10 00:15:16 +0700

    Добавьте файлы проекта.

---
+++ Project 4/Project 4.cpp
@@ -0,0 +1,38 @@
+﻿#include <stdio.h>
+#include <conio.h>
+#include <string.h>
+struct Item
+{
+	char name[50];
+	int cost;
+	void inititem(char s[50], int a);
+};
+void Item::inititem(char s[50], int a) {
+	strcpy(name, s);
+	cost = a;
+};
+
+
+struct compose
+{
+	Item *a;
+	int colvo;
+	int sostav[];
+	void initcompose(Item *m, int a, int mas[20]);
+}; 
+struct sold
+{
+	Item a;
+	int b;
+	void initsold(Item a, int b);
+};
+struct action
+{
+	resource a;
+	void initdel(resource a, int b);
+	void initsup(resource a, int b);
+}; 
+struct resource
+{
+	int key, kolvo;
+};

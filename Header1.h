745a2f3d59001eb7c6a9047e3639590f5206432d
Alex <panins895@gmail.com>
2023-12-12 21:01:47 +0700

    #

--- Header1.h
+++ Header1.h
@@ -1,51 +1,175 @@
 #pragma once
 #include <iostream>
 #include <string>
-#include <stdio.h>
 #include <vector>
 
 
 using namespace std;
 
-struct resource
-{
+class resource {
+private:
 	string name;
-	unsigned int kolvo;
+	double kolvo;
+public:
+	string getname() {
+		return name;
+	}
+	unsigned int getkol() {
+		return kolvo;
+	}
+	void setresource(string imya, double kolich, vector <resource*> *sklad) {
+		name = imya;
+		kolvo = kolich;
+		sklad->push_back(this);
+	}
+	string showres() {
+		string outp;
+		outp = this->name + " : " + to_string(this->kolvo) + "\n";
+		return outp;
+	}	
+	void consresinit(vector <resource*> *sklad) {
+		cout << "Input resource name:";
+		cin >> this->name;
+		cout << "Input resource quantity:";
+		cin >> this->kolvo;
+		sklad->push_back(this);
+	}
+	void reducekol(double a) {
+		kolvo -= a;
+	}
+	void upkol(double a) {
+		kolvo += a;
+	}
 };
-struct compose
+
+class compose
 {
+public:
 	vector <resource> sost;
-	vector <unsigned int> n;
+	unsigned int k;
+	vector <double> n;
+	void setcompose(vector <resource> cons, vector <double> m) {
+		int i = 0;
+		for (i; i < cons.size(); i++) {
+			sost.push_back(cons[i]);
+			n.push_back(m[i]);
+		}k = cons.size();
+	}
+	unsigned int getlen() {
+		return k;
+	}void reduselen(unsigned int a) {
+		k-=a;
+	}
+	void uplen() {
+		k+=1;
+	}
 };
-struct Item
+
+class Item
 {
+private:
 	string name;
 	unsigned int cost;
-	compose *p;
+	compose sost;
+public:
+	void setitem(string imya, unsigned int price, compose *n, vector <Item*> *menu) {
+		name = imya;
+		cost = price;
+		sost = *n;
+		menu->push_back(this);   
+	}
+	string showsost() {
+		string outp="";
+		for (int i = 0; i < this->sost.n.size(); i++) {
+			outp += (this->sost.sost[i].getname() + ": " + to_string(this->sost.n[i]) + "\n");
+		}return outp;
+	}
+	unsigned int getcost() {
+		return this->cost;
+	}
+	string getname() {
+		return name;
+	}
+	compose* getsost() {
+		return &sost;
+	}
+	void costchange(unsigned int b) {
+		cost = b;
+	}
+	void sostadd( resource res, double k) {
+		this->sost.sost.push_back(res);
+		this->sost.n.push_back(k);
+		this->sost.uplen();
+	}
+	void sostdel(int b) {
+		for (int i = 0; i < b; i++) {
+			this->sost.sost.pop_back();
+			this->sost.n.pop_back();
+		}this->sost.reduselen(b);
+	}
 };
-struct sold
+
+class sold
 {
+private:
 	Item item;
 	unsigned int kolvo;
 	int profit;
+public:
+	void setsold(Item coffee, unsigned int kolich, vector <sold> *soldhist) {
+		item = coffee;
+		kolvo = kolich;
+		profit = kolich * coffee.getcost();
+		for (int i = 0; i < item.getsost()->getlen();i++) {
+			item.getsost()->sost[i].reducekol(item.getsost()->n[i] * kolvo);
+		}soldhist->push_back(*this);		
+	}string soldinfo(vector <sold> soldhist) {
+		int summ=0;
+		string outp = "";
+		for (int i = 0; i < soldhist.size(); i++) {
+			outp += soldhist[i].item.getname() + ": " + to_string(soldhist[i].kolvo) + " = " + to_string(soldhist[i].profit) + "\n";
+			summ += soldhist[i].profit;
+		}outp += "profit:" + to_string(summ)+"\n"; return outp;
+	}
 };
-struct action
+class action
 {
-	resource resource;
-	unsigned int kolvo;
-	bool f;
+private:
+	resource res;
+	int kolvo;
+	int f;
+public:
+	int getacttype() { return f; }
+	void setaction(resource a, unsigned int b, bool flag, vector <action> *acthist) {
+		res = a;
+		kolvo = b;
+		f = flag;
+		acthist->push_back(*this);
+		if (f) {
+			res.upkol(kolvo);
+		}
+		else { res.reducekol(kolvo); }
+	}
+	string acthist() {
+		string outp = "";
+		if (this->getacttype() == 1) {
+				outp += res.getname() + ": " + to_string(kolvo) + "\n";
+			}
+			else { outp += res.getname() + ": " + to_string(kolvo * (-1)) + "\n"; }
+		return outp;
+	}
 };
-Item inititem(string name, unsigned int cost, compose a);
-resource initresource(string name, unsigned int kolvo);
-sold initsold(Item item, unsigned int kolvo);
-action initaction(resource a, unsigned int b, bool f);
-compose initcompose(vector<resource> m, vector <unsigned int> n);
-void showres(vector<resource> m);
-void showmenu(vector <Item> m);
-void acthist(vector <action> m);
-void showing(Item p);
-void solddata(vector <sold> m);
-void costchange(Item p, unsigned int b);
-void sostadd(compose comp, resource r, unsigned int b);
-void sostdel(compose comp, unsigned int b);
-resource consresinit();
+//Item inititem(string name, unsigned int cost, compose a);
+//resource initresource(string name, unsigned int kolvo);
+//sold initsold(Item item, unsigned int kolvo);
+//action initaction(resource a, unsigned int b, bool f);
+//compose initcompose(vector<resource> m, vector <unsigned int> n);
+//void showres(vector<resource> m);
+//void showmenu(vector <Item> m);
+//void acthist(vector <action> m);
+//void showing(Item p);
+//void solddata(vector <sold> m);
+//void costchange(Item p, unsigned int b);
+//void sostadd(compose comp, resource r, unsigned int b);
+//void sostdel(compose comp, unsigned int b);
+//resource consresinit();
--- Test.cpp
+++ Test.cpp
@@ -1,51 +1,51 @@
 #include "Header1.h"
 #include <iostream>
 #include <vector>
-
+#include <stdio.h>
+#include <map>
 using namespace std;
 
 int main() {
-	resource coffee = initresource("coffee", 1000);
-	resource milk = initresource("milk", 1000);
-	resource sugar = initresource("sugar", 1000);
-	resource sirop_chocolate = initresource("sirop_chocolate", 1000);
-	resource chocolate = initresource("chocolate", 1000);
-	resource sirop_nuts = initresource("sirop_nuts", 1000);
-	resource sirop_cherry = initresource("sirop_cherry", 1000);
-	resource cacao = initresource("cacao", 1000);
+	std::vector<resource*> sclad = {};
+	std::vector<Item*> menu = {};
+	std::vector<action> actions = {};
+	std::vector<sold> soldhist = {};
+	resource milk, coffee, sugar, chokolate, sirop;
+	milk.setresource("milk", 30, &sclad);
+	chokolate.setresource("chokolate", 200, &sclad);
+	coffee.setresource("coffee", 50, &sclad);
+	sirop.setresource("sirop", 20, &sclad);
+	sugar.consresinit(&sclad);
+	std::vector<resource> cof = { coffee, milk, sugar };
+	std::vector<double> cs = { 0.060, 0.090, 0.060 };
+	std::vector<double> cb = { 0.10, 0.150, 0.10 };
+	std::vector<double> am = { 0.15, 0.0, 0.060 };
+	compose capuch_small, capuch_big, amer;
+	capuch_small.setcompose(cof, cs);
+	capuch_big.setcompose(cof, cb);
+	amer.setcompose(cof, am);
+	Item cappuchino_small, cappuchino_big, americano;
+	cappuchino_big.setitem("cappuchino_big", 180, &capuch_big, &menu);
+	cappuchino_small.setitem("cappuchino_small", 100, &capuch_small, &menu);
+	americano.setitem("americano", 120, &amer, &menu);
+	cout << milk.showres();
+	cout << cappuchino_big.showsost();
+	cappuchino_big.costchange(220);
+	cappuchino_big.sostdel(1);
+	cappuchino_big.sostadd(sirop, 0.03);
+	cout << cappuchino_big.showsost();
+	cout << to_string(cappuchino_big.getcost())+"\n";
 
-	std::vector<resource> cs = { coffee, milk, sugar};
-	std::vector<unsigned int> csn = {30, 90, 30};
-	compose comp_capuchino_small = initcompose(cs, csn);
-	Item capuchino_small = inititem("capuchino_small", 150, comp_capuchino_small);
-	sold cap_small = initsold(capuchino_small, 4);
+	sold one, two;
+	one.setsold(cappuchino_big, 5, &soldhist);
+	two.setsold(americano, 7, &soldhist);
+	cout<<one.soldinfo(soldhist);
+	cout << milk.showres();
 
-	std::vector<resource> hc = { chocolate, milk, sugar };
-	std::vector<unsigned int> hcn = { 50, 90, 30 };
-	compose comp_hot_chocolate = initcompose(hc, hcn);
-	Item hot_chocolate = inititem("hot_chocolate", 200, comp_hot_chocolate);
-	sold hot_choc = initsold(hot_chocolate, 7);
-
-	action one = initaction(coffee, 100, 1);
-	action two = initaction(milk, 150, 0);
-
-	std::vector<resource> sclad = { coffee, milk, sugar, sirop_cherry, chocolate, sirop_chocolate, sirop_nuts, cacao };
-	std::vector<Item> menu = {capuchino_small, hot_chocolate};
-	std::vector<action> acts = {one, two};
-	showres(sclad);
-	showmenu(menu);
-	acthist(acts);
-	showing(capuchino_small);
-	std::vector<sold> mondaysold = { hot_choc, cap_small };
-	solddata(mondaysold);
-
-	sostdel(comp_capuchino_small, 1);
-	sostadd(comp_capuchino_small, sirop_cherry, 20);
-	costchange(capuchino_small, 180);
-	showing(capuchino_small);
-	sold cherry_small_cap = initsold(capuchino_small, 10);
-	std::vector<sold> demonstrate = { cherry_small_cap };
-	solddata(demonstrate);
-	showres(sclad);
-	resource newres = consresinit();
+	action first, second;
+	first.setaction(milk, 2, 0, &actions);
+	second.setaction(chokolate, 10, 1, &actions);
+	cout << second.acthist();
+	cout << first.acthist();
+	cout << milk.showres();
 }
